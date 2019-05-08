4ff7413aee6a9a388480c66efe381c21d8874cb6
niorth <hugo.niort9@gmail.com>
2019-05-08 21:14:53 +0200

    switch to django user Model

--- webProjectIG3/cookbook/admin.py
+++ webProjectIG3/cookbook/admin.py
@@ -3,7 +3,6 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import User
 from .models import Recipe
 from .models import Contains
 from .models import Ingredient
@@ -12,7 +11,6 @@ from .models import RecipeTag
 
 # Register your models here.
 
-admin.site.register(User)
 admin.site.register(Recipe)
 admin.site.register(Contains)
 admin.site.register(Ingredient)
--- webProjectIG3/cookbook/models.py
+++ webProjectIG3/cookbook/models.py
@@ -2,17 +2,9 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-
-class User(models.Model):
-	name = models.CharField(max_length = 100)
-	firstname = models.CharField(max_length = 100)
-	email = models.EmailField(max_length = 100)
-	password = models.CharField(max_length = 100)
-
-	def __str__(self):
-		return self.name
 
 class Recipe(models.Model):
 	user = models.ForeignKey(User, on_delete = models.CASCADE)
