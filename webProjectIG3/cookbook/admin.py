37643282f714cf024c748d1039c056d586add38e
niorth <hugo.niort9@gmail.com>
2019-05-08 17:40:13 +0200

    cookbook models

--- webProjectIG3/cookbook/admin.py
+++ webProjectIG3/cookbook/admin.py
@@ -3,4 +3,18 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from .models import User
+from .models import Recipe
+from .models import Contains
+from .models import Ingredient
+from .models import Belongs
+from .models import RecipeTag
+
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Recipe)
+admin.site.register(Contains)
+admin.site.register(Ingredient)
+admin.site.register(Belongs)
+admin.site.register(RecipeTag)
--- webProjectIG3/cookbook/models.py
+++ webProjectIG3/cookbook/models.py
@@ -4,3 +4,57 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+	name = models.CharField(max_length = 100)
+	firstname = models.CharField(max_length = 100)
+	email = models.EmailField(max_length = 100)
+	password = models.CharField(max_length = 100)
+
+	def __str__(self):
+		return self.name
+
+class Recipe(models.Model):
+	user = models.ForeignKey(User, on_delete = models.CASCADE)
+	name = models.CharField(max_length = 100)
+	recipe_text = models.TextField()
+	number_of_people = models.IntegerField()
+
+	def __str__(self):
+		return self.name
+
+class Ingredient(models.Model):
+	name = models.CharField(max_length = 100)
+
+	def __str__(self):
+		return self.name
+
+class Contains(models.Model):
+	ingredient = models.ForeignKey(Ingredient, on_delete = models.CASCADE)
+	recipe = models.ForeignKey(Recipe, on_delete = models.CASCADE)
+	unit = models.CharField(max_length = 100)
+	quantity = models.IntegerField()
+
+	class Meta:
+		unique_together = (('ingredient', 'recipe'), )
+
+class RecipeTag(models.Model):
+	name = models.CharField(max_length = 100)
+
+	def __str__(self):
+		return self.name
+
+class Belongs(models.Model):
+	recipeTag = models.ForeignKey(RecipeTag, on_delete = models.CASCADE)
+	recipe = models.ForeignKey(Recipe, on_delete = models.CASCADE)
+
+	class Meta:
+		unique_together = (('recipeTag', 'recipe'), )
+
+ 
+
+
+
+
+
+
