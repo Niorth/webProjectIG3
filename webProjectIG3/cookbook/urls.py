46237f21d49733908ff134e9eb7735d5b68b7bd3
niorth <hugo.niort9@gmail.com>
2019-05-09 18:41:04 +0200

    Login + join

--- webProjectIG3/cookbook/urls.py
+++ webProjectIG3/cookbook/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^authentification/$', views.authentification, name='authentification'),
     url(r'^login/$', views.loginView, name='login'),
+    url(r'^join/$', views.joinView, name='join'),
+    url(r'^register/$', views.register, name='register'),
 ]
--- webProjectIG3/cookbook/views.py
+++ webProjectIG3/cookbook/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.views import generic
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 from django.urls import reverse
 
 # Create your views here.
@@ -15,6 +16,9 @@ def index(request):
 def loginView(request):
 	return render(request, 'cookbook/login.html')
 
+def joinView(request):
+	return render(request, 'cookbook/join.html')
+
 def authentification(request):
 	user = authenticate(username = request.POST['username'], password = request.POST['password'])
 	if user is not None:
@@ -23,3 +27,46 @@ def authentification(request):
 
 	else:
 		return render(request, 'cookbook/login.html', {'error_message' : "Nom d'utilisateur ou mot de passe incorrecte"})
+
+def register(request):
+	username = request.POST['username']
+	name = request.POST['name']
+	firstname = request.POST['firstname']
+	email = request.POST['email']
+	password = request.POST['password']
+
+	if(username == "" or name == "" or firstname == "" or email == "" or password == ""):
+		return render(request, 'cookbook/join.html', {
+			'error_message' : "Veuillez remplir tous les champs",
+			'username' : username,
+			'name' : name,
+			'firstname' : firstname,
+			'email' : email,
+		})
+
+	if(not(User.objects.filter(username = username).exists())):
+		if(len(password) > 5):
+			#create user
+			user = User.objects.create_user(username, email, password)
+			user.last_name = name
+			user.first_name = firstname
+			user.save()
+			login(request, user)
+			return HttpResponseRedirect(reverse('cookbook:index'))
+		else:
+			#wrong password
+			return render(request, 'cookbook/join.html', {
+				'error_message' : "Votre mot de passe doit contenir au moins 6 caracteres",
+				'username' : username,
+				'name' : name,
+				'firstname' : firstname,
+				'email' : email,
+				})
+	else:
+		#wrong username
+		return render(request, 'cookbook/join.html', {
+			'error_message' : "Nom d'utilisateur indisponible",
+			'name' : name,
+			'firstname' : firstname,
+			'email' : email,
+			})
