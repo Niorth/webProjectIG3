63c25c010d25b462e2f514ed2767c6277bc2e5d7
niorth <hugo.niort9@gmail.com>
2019-05-09 09:11:10 +0200

    login + bootstrap

--- webProjectIG3/cookbook/urls.py
+++ webProjectIG3/cookbook/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'cookbook'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^authentification/$', views.authentification, name='authentification'),
+    url(r'^login/$', views.loginView, name='login'),
 ]
--- webProjectIG3/cookbook/views.py
+++ webProjectIG3/cookbook/views.py
@@ -2,9 +2,24 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views import generic
+from django.contrib.auth import authenticate, login
+from django.urls import reverse
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    return render(request, 'cookbook/index.html')
+
+def loginView(request):
+	return render(request, 'cookbook/login.html')
+
+def authentification(request):
+	user = authenticate(username = request.POST['username'], password = request.POST['password'])
+	if user is not None:
+		login(request, user)
+		return HttpResponseRedirect(reverse('cookbook:index'))
+
+	else:
+		return render(request, 'cookbook/login.html', {'error_message' : "Nom d'utilisateur ou mot de passe incorrecte"})
